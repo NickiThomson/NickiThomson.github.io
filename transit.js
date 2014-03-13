0f11a2d3a7e401230a7e2f2cadc539b0259fa17d
Nicki Thomson <nicki.r.thomson@gmail.com>
2014-03-13 00:36:41 -0400

    trying to parse data

--- transit.js
+++ transit.js
@@ -154,8 +154,13 @@
    			 	linecolor = tstopsData['line'];
 
    			 	for (var i=0; i<stations[linecolor].length; i++){
-
+   			 		var mark = new google.maps.LatLng(stations[i]['lat'], stations[i]['long']);
    			 	}
+   			 	google.maps.event.addListener(mark, 'click', function() {
+					infowindow.close();
+					infowindow.setContent(stations[i]['stop']);
+					infowindow.open(map, this);
+				}
    			 }
 
 
