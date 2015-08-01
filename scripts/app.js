dffda09ca96b6906cddceec2db375f5734ea495c
Nicki Thomson <nicki.r.thomson@gmail.com>
2015-08-01 12:38:25 -0700

    uppdated bio

--- scripts/app.js
+++ scripts/app.js
@@ -30,12 +30,12 @@ app.run(function($rootScope, $location, $anchorScroll, $routeParams) {
 
 app.controller('carouselController', function($scope){
 	  $scope.myInterval = 5000;
-	  var numSlides = 4;
+	  var numSlides = 3;
 	  var slides = $scope.slides = [];
 	  $scope.addSlide = function() {
 	    slides.push({
-	      image: ['files/photos/opera.jpg', 'files/photos/aoii.jpg', 'files/photos/meanGirl.jpg', 'files/photos/ballroom.jpg'][slides.length % numSlides],
-	      text: ['Oprea Ensemble Performance of "Captain Lovelock", April 2014', 'Proud member of Alpha Omicron Pi', 'Sumo-robot "mean girl"', 'Tufts Ballroom Competition 2013'][slides.length % numSlides]
+	      image: ['files/photos/swarmbotOnCourse.jpg', 'files/photos/aoii.jpg', 'files/photos/meanGirl.jpg'][slides.length % numSlides],
+	      text: ['Junior Design Project - Swarmbots', 'Proud member of Alpha Omicron Pi', 'Sumo-robot "Mean Girl"'][slides.length % numSlides]
 	    });
 	  };
 	  for (var i=0; i<numSlides; i++) {
@@ -46,9 +46,9 @@ app.controller('carouselController', function($scope){
 app.controller('bioController', function($scope){
   $scope.bioText = {
     content: [
-    "Nicki Thomson is currently a junior at Tufts University studying Computer Engineering, (which for those who don’t know, by the Tufts curriculum, is what you get when Electrical Engineering and Computer Science get together and have a baby). Additionally she is a member of a variety of campus groups, including Tufts Robotics Club, The Tufts Opera Ensemble, Alpha Omicron Pi, and Nicki is the Social Coordinator of the Tufts Ballroom Team.",
-  "Nicki spent last summer as a software developer intern at IBM, helping to redesign the Emptoris Suite product’s log in workflow. This helped her to gain new web programming skills such as Bootstrap and AngularJS, as well as develop her existing ones. Nicki really enjoyed the vast amount of learning that came with this job, and hopes to be able to pursue more internships which allow her to learn so much.",
+    "Nicki Thomson is currently a Senior at Tufts University studying Computer Engineering, (which for those who don’t know, by the Tufts curriculum, is what you get when Electrical Engineering and Computer Science get together and have a baby). On campus she is involved with the Tufts Opera Ensemble, Alpha Omicron Pi, and she is a TA for Introduction to Computer Science. After her Senior year Nicki will be staying at Tufts for an additional year in order to get her Master’s degree in Electrical Engineering.",
+  "Nicki spent last summer as a Software Engineer Intern at Microsoft on the Azure SQL DB Management Service team. During her internship Nicki learned C# and also learned about software design patterns, as well as learning about the lifecycles of servers. Nicki really enjoyed the vast amount of learning that came with this job, and hopes to be able to pursue another internship, and eventually a career, which allow will her to learn so much.",
   "Prior to Tufts, Nicki attended six years of boarding school on a cliffside in England, where she completed her A Levels in Physics, Mathematics and Theatre Studies. During her time in England she also completed her Duke of Edinburgh’s Gold award, which is achieved with a long time commitment to a sport, a skill and a service project, as well as including a residential trip, and an outdoor expedition.",
-  "Nicki is currently looking for summer employment for the summer of 2015, she hopes to gain a position which will give her more insight into Computer Engineering, and helps to give her an idea what she might like to study at grad school after her graduation in 2016."
+  "Nicki is currently looking for a firmware internship for the summer of 2016, as she is strongly considering a career in firmware development after she finishes grad school in 2017."
   ]};
 });
