fcaa7ec4c69f5fde25d2bb279a6cb0568ec60b68
Nicki Thomson <nicki.r.thomson@gmail.com>
2015-08-01 10:31:53 -0700

    so much better front page in mobile

--- scripts/resumeController.js
+++ scripts/resumeController.js
@@ -1,90 +1,90 @@
 var app = angular.module('nickiApp');
 	
 app.controller('resumeController', function($scope){
-		$scope.categories = {
-			education : "Education",
+	$scope.categories = {
+		education : "Education",
 
-			classes : {
-				title : "Relevant Coursework",
+		classes : {
+			title : "Relevant Coursework",
 
-				engineering: {
-					title: "Engineering",
-					items: [
-						"Microprocessor Architecture And Applications",
-						"Linear Systems",
-						"Intro to Digital Logic Circuits",
-						"Electronics 1",
-						"Probabilistic Systems Analysis",
-						"Engineering Graphics",
-						"Simple Robotics",
-						"Intro to Electrical Systems",
-						"Intro to Mechanics Statics and Dynamics"
-
-					]
-				},
-				computerScience : {
-					title: "Computer Science",
-					items: [
-						"Data structures",
-						"Web Programming",
-						"Discrete Mathematics",
-						"Computational Theory"
-
-					]
-				},
-				mathematics : {
-					title : "Mathematics",
-					items: [
-						"Multivariable Calculus",
-						"Differential Equations"
-					]
-				}
+			engineering: {
+				title: "Engineering",
+				items: [
+					"Microprocessor Architecture And Applications",
+					"Digital Logic Systems",
+					"Linear Systems",
+					"Electronics 1",
+					"Electromagnetic Fields and Waves",
+					"Probabilistic Systems Analysis",
+					"Engineering Graphics",
+					"Simple Robotics",
+					"Intro to Electrical Systems",
+					"Intro to Mechanics Statics and Dynamics"
+				]
 			},
-				skills : {
-					title : "Skills",
-					software: {
-						title : "Software", 
-						items : [
-							"LTSPICE",
-							"Solidworks",
-							"Autodesk Inventor",
-							"AutoCAD",
-							"MathCAD"
-						]
-				},
-				programmingLanguages : {
-					title : "Programming Languages",
-					items : [
-						"MATLAB",
-						"C++",
-						"Visual Basic",
-						"LabView"
-					]
-				},
-				webDevelopment : {
-					title : "Web Development",
-					items : [
-						"HTML",
-						"CSS",
-						"JavaScript",
-						"Bootstrap",
-						"AngularJS"
-					]
-				}
+			computerScience : {
+				title: "Computer Science",
+				items: [
+					"Data structures",
+					"Web Programming",
+					"Discrete Mathematics",
+					"Computational Theory"
+				]
 			},
-			projects : {
-				title : "Projects"
-			},
-			experience : {
-				title : "Experience"
-			},
-			awards : {
-				title : "Awards"
-			},
-			activities : {
-				title : "Activities/ Interests"
+			mathematics : {
+				title : "Mathematics",
+				items: [
+					"Multivariable Calculus",
+					"Differential Equations"
+				]
 			}
+		},
+			skills : {
+				title : "Skills",
+				software: {
+					title : "Software", 
+					items : [
+						"LTSPICE",
+						"Solidworks",
+						"Autodesk Inventor",
+						"AutoCAD",
+						"MathCAD"
+					]
+			},
+			programmingLanguages : {
+				title : "Programming Languages",
+				items : [
+					"C++",
+					"C#",
+					"MATLAB",
+					"Python",
+					"Visual Basic",
+					"LabView"
+				]
+			},
+			webDevelopment : {
+				title : "Web Development",
+				items : [
+					"HTML",
+					"CSS",
+					"JavaScript",
+					"Bootstrap",
+					"AngularJS"
+				]
+			}
+		},
+		projects : {
+			title : "Projects"
+		},
+		experience : {
+			title : "Experience"
+		},
+		awards : {
+			title : "Awards"
+		},
+		activities : {
+			title : "Activities/ Interests"
+		}
 
-		};
-
+	};
 });
